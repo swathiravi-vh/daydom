217a960240d933b4444b5bb17dc6290e4de32b61
swathi ravi <110903083+swathiravi-vh@users.noreply.github.com>
2022-09-01 15:01:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function foo(){
+    var re=document.getElementById("main").value;
+    console.log(re);
+    var r=document.getElementById("main1").value;
+    console.log(r);
+    var l=document.getElementById("main2").value;
+    console.log(l);
+    var e=document.getElementById("main3").value;
+    console.log(e);
+
+}
